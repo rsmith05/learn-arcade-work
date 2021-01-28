e231c7e217ae66a95ee56ada323d870f3d974352
Ronan Smith <ronansmith@outlook.com>
2021-01-28 13:30:29 -0800

    Finished lab 1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,2 +1,8 @@
-print("here are multiple ways to print on other lines")
-print("first we got the ")
+print("Here are multiple ways to print on other lines.")
+print("First we got the double print functions which is what I just did.")
+print("Now\nwe\ngot\nthe\n\\n\nway\nof\ndoing\nit\nalso known as a going down a linefeed.")
+print("""The final one
+is what I like to call
+\"The Triple Thing,\"
+otherwise known as Triple Quotes""")
+print("\nok that was everything...")
