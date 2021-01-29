bc44f87615569b337ddd66c4eddcbc84a566c54e
Ronan Smith <ronansmith@outlook.com>
2021-01-29 10:53:10 -0800

    lab 1 challenge

---
+++ Lab 01 - First Program/Lab_01_Challenge.py
@@ -0,0 +1,6 @@
+print("I am going to sleep a lot,")
+print("and a lot of short naps.")
+print("can't forget playing\nsome videogames like ususal\ngonna finsish Omori for the second time.")
+print("""Last but not least,
+start my english essay I should have started 
+a week ago.""")
