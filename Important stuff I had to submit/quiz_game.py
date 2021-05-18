b2af46eb9657a0afbdd8e3ea581d5209541ee7f2
Ronan Smith <ronansmith@outlook.com>
2021-05-17 21:56:37 -0700

    Finish quiz game

--- Important stuff I had to submit/quiz_game.py
+++ Important stuff I had to submit/quiz_game.py
@@ -1,6 +1,7 @@
 def main(answers_correct):
     print("Quiz game")
     print()
+
     # FIRST QUESTION
     question_1 = input("What pokemon is a fire type?\nA. Bulbasaur"
                        "\nB. Charmander\nC. Altaria\nD. Greninja\n? ")
@@ -9,8 +10,8 @@ def main(answers_correct):
         answers_correct += 1
     else:
         print("Incorrect!")
-
     print()
+
     # SECOND QUESTION
     question_2 = input("What is the airspeed velocity of a European unladen swallow?"
                        "\nA. 26 mi/h\nB. 20 mi/h\nC. 10 mi/h\nD. 24 mi/h\n? ")
@@ -19,8 +20,8 @@ def main(answers_correct):
         answers_correct += 1
     else:
         print("Incorrect!")
-
     print()
+
     # THIRD QUESTION
     question_3 = int(input("What is the vertical asymptote of the equation "
                            "(x-2)/(x+4)? "))
@@ -29,8 +30,8 @@ def main(answers_correct):
         answers_correct += 1
     else:
         print("Incorrect!")
-
     print()
+
     # FOURTH QUESTION
     question_4 = int(input("what is the zero for the same equation above? "))
     if question_4 == 2:
@@ -38,8 +39,8 @@ def main(answers_correct):
         answers_correct += 1
     else:
         print("Incorrect!")
-
     print()
+
     # FIFTH QUESTION
     question_5 = input("Am I asking this question because I'm too lazy to think of "
                        "another question (YES/NO)? ")
